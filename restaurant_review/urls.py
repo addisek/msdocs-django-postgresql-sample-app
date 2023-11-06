839c613c594a1148bbeedfc1390746a9c731116a
addisek <147247647+addisek@users.noreply.github.com>
2023-11-06 09:37:21 +0100

    Update urls.py

--- restaurant_review/urls.py
+++ restaurant_review/urls.py
@@ -1,11 +1,10 @@
+# w pliku urls.py
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:id>/', views.details, name='details'),
-    path('create', views.create_restaurant, name='create_restaurant'),
-    path('add', views.add_restaurant, name='add_restaurant'),
-    path('review/<int:id>', views.add_review, name='add_review'),
+    path('login/', views.login_view, name='login'),
+    path('contact/', views.contact_view, name='contact'),
+    # inne ścieżki
 ]
